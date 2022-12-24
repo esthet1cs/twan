e23f001db1e68f550e7fa83cb111454933d5dbcd
cmcx <cms@dock.in-berlin.de>
2022-12-24 23:40:32 +0100

    added support for save_dir attribute to twitstream

--- twitstream.py
+++ twitstream.py
@@ -35,6 +35,7 @@ if args.credentials:
     twan_credentials = args.credentials
 else:
     twan_dir = os.path.expanduser('~') + '/.twan/'
+    twan_credentials = twan_dir + '/credentials'
 
 with open(twan_credentials, 'r') as f:
     credentials = [line.strip() for line in f if line.strip() != '']    # read the credentials line by line
@@ -44,6 +45,12 @@ for cred in credentials:                        # iterate over the four entries
     cred = cred.split('=')                      # split the line into name and value
     creds[cred[0].strip()] = cred[1].strip()    # add it to the credentials store
 
+# define datafile
+if args.save_dir:
+    if args.save_dir[-1] == '/':
+        datafile = args.save_dir + 'datastream.jsonl'
+    else:
+        datafile = args.save_dir + '/datastream.jsonl'
 
 def authenticate(consumer_key, consumer_secret, access_token, access_token_secret):
     '''
@@ -60,7 +67,7 @@ class MyListener(StreamListener):
  
     def on_data(self, data):
         try:
-            with open('datastream.json', 'a') as f:
+            with open(datafile, 'a') as f:
                 f.write(data)
                 return True
         except BaseException as e:
